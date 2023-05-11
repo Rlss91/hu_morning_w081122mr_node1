2c5950ccbae3be3303f4e23cc617abbcf3a19b7f
Rlss91 <rlss91@gmail.com>
2023-05-11 10:21:48 +0300

    req and build in mw

--- app.js
+++ app.js
@@ -41,8 +41,31 @@ app.use("/mw2", (req, res, next) => {
 app.get("/", (req, res) => {
   //   res.send("response from server");
   //   res.json("response from server");
+  console.log("headers", req.headers);
+  console.log("qparams", req.query);
   res.status(404).json("error from server");
   //   res.sendFile(path.join(__dirname, "Untitled.png"));
+});
+
+app.use("/:id/:name", (req, res, next) => {
+  console.log(req.params);
+  if (req.params && req.params.name && req.params.name.startsWith("k")) {
+    req.user = {
+      msg: "ok",
+    };
+  }
+  next();
+});
+
+app.get("/:id/:name", (req, res) => {
+  console.log("is user exists", req.hasOwnProperty("user"));
+  console.log("user", req.user);
+  console.log("params", req.params);
+  // console.log(req);
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
 });
 
 // app.use("/", indexRouter);
