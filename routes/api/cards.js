fe14571cdb6b4a2f6187414024bc17e245f592ef
Rlss91 <rlss91@gmail.com>
2023-05-14 15:52:57 +0300

    joi

--- routes/api/cards.js
+++ routes/api/cards.js
@@ -1,12 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const cardsServiceModel = require("../../model/cards/cardsService");
+const cardsValidationService = require("../../validation/cardsValidationService");
 
 router.post("/", async (req, res) => {
   try {
-    //!joi validation
+    await cardsValidationService.createCardValidation(req.body);
     const dataFromMongoose = await cardsServiceModel.createCard(req.body);
     console.log("dataFromMongoose", dataFromMongoose);
+    res.json({ msg: "ok" });
   } catch (err) {
     console.log(err);
     res.status(400).json(err);
