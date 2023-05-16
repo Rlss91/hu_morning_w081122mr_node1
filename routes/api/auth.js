a6cc9c8159e8120c0b32a738b183450017c5e002
Rlss91 <rlss91@gmail.com>
2023-05-16 16:20:10 +0300

    5

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -2,12 +2,24 @@ const express = require("express");
 const router = express.Router();
 
 //http://localhost:8181/api/auth/register
-router.get("/register", (req, res) => {
-  res.json({ msg: "register" });
+router.post("/register", async (req, res) => {
+  try {
+    /*
+     * joi
+     * email unique
+     * encrypt the password
+     * normalize
+     * create user
+     * response user created
+     */
+    // res.json({ msg: "register" });
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 //http://localhost:8181/api/auth/login
-router.get("/login", (req, res) => {
+router.post("/login", (req, res) => {
   res.json({ msg: "login" });
 });
 
